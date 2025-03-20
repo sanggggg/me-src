99bd7ef5088e2c507ac98549fc687f6a9c12e166
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-03-21 01:35:45 +0900

    add toggle ui

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import SEO from "./seo";
+import { Link } from "gatsby";
 
 export const LocaleContext = React.createContext<{
   locale: string;
@@ -14,13 +15,21 @@ const Layout: React.FC<
     lang: string;
   }>
 > = ({ pageTitle, isArticle, children, lang }) => {
+  const currentPath =
+    typeof window !== "undefined" ? window.location.pathname : "";
+  const otherLang = lang === "ko" ? "en" : "ko";
+  const otherLangPath = currentPath.replace(`/${lang}/`, `/${otherLang}/`);
+
   return (
     <LocaleContext.Provider value={{ locale: lang }}>
-      <article className="container prose prose-sm md:prose dark:prose-dark">
-        <SEO isArticle={isArticle ?? false} title={pageTitle} />
-        <h1 style={{ fontWeight: 700 }}>{pageTitle}</h1>
-        {children}
-      </article>
+      <div className="relative">
+        <article className="container prose prose-sm md:prose dark:prose-dark">
+          <Link to={otherLangPath}>{lang === "ko" ? "English" : "한국어"}</Link>
+          <SEO isArticle={isArticle ?? false} title={pageTitle} />
+          <h1 style={{ fontWeight: 700 }}>{pageTitle}</h1>
+          {children}
+        </article>
+      </div>
     </LocaleContext.Provider>
   );
 };
